97a1474c2fa82bf3da6bcd1a4cb93d35542b5c5d
lisdavi <153983285+lisdavi@users.noreply.github.com>
2023-12-25 19:47:21 +0300

    Решён конфликт слияния

--- plm/plm/file_reader.h
+++ plm/plm/file_reader.h
@@ -1,1 +1,10 @@
 #pragma once
+#ifndef PHONE_CALL_READER_H
+#define PHONE_CALL_READER_H
+
+#include "const.h"
+#include "tele_cons.h"
+
+void read(const char* file_name, phone_call* array[], int& size);
+
+#endif
