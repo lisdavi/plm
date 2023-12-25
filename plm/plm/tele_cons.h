9554ebd11b6ed5b1a4508181d1b2f000428e78dc
lisdavi <153983285+lisdavi@users.noreply.github.com>
2023-12-25 19:27:40 +0300

    Добавлены структуры для хранения данных

--- plm/plm/tele_cons.h
+++ plm/plm/tele_cons.h
@@ -1,1 +1,32 @@
 #pragma once
+#pragma once
+#include "const.h"
+#ifndef PHONE_CALL_H
+#define PHONE_CALL_H
+
+
+
+struct date 
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct times
+{
+    int hours;
+    int minutes;
+    int seconds;
+};
+struct phone_call
+{
+    int phone_number;
+    times call_time;
+    date call_date;
+    times duration;
+    char tariff[TARIFF_SIZE];
+    float cost;
+};
+
+#endif
