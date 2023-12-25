b6eec6124968ea4a4c59cd3fc032ec15549efaed
lisdavi <153983285+lisdavi@users.noreply.github.com>
2023-12-25 19:08:55 +0300

    main.cpp
    "Добавлен вывод общей информации о задании"

--- plm/plm/main.cpp
+++ plm/plm/main.cpp
@@ -6,6 +6,6 @@ int main()
 {
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #9.Telephone conversations\n";
-    cout << "Author: Anastasia Nabieva\n";
+    cout << "Author:  Anastasia Nabieva\n";
     return 0;
 }
